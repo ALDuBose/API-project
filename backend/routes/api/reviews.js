8b58b10cafe0ea2eb5ee1b439d58a5904b4380b1
ALDuBose <alr.dubose@gmail.com>
2023-05-10 22:14:47 -0500

    local testing for 50 completed

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -16,29 +16,39 @@ const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
 
 router.post("/:reviewId/images", requireAuth, async (req, res, next) => {
-
   const { reviewId } = req.params;
   const { user } = req;
   const { url } = req.body;
   let result = {};
 
-  // authenticate, and check on post, shouldn't create if Booking didn't exist
-
   const reviewData = await Review.findOne({
     where: { id: reviewId },
+    include: [
+      { model: Spot, attributes: ["id", "ownerId"] },
+      { model: ReviewImage, attributes: ["id", "url"] },
+    ],
   });
-  if (!reviewData) return res.status().json();
+  if (!reviewData)
+    return res.status(404).json({
+      message: "Spot couldn't be found",
+    });
+  if (user.id !== reviewData.Spot["ownerId"])
+    return res.status(403).json({ message: "Forbidden" });
+  let urlData = reviewData.toJSON();
+  if (urlData.ReviewImages.length >= 10)
+    return res.status(403).json({
+      message: "Maximum number of images for this resource was reached",
+    });
 
   const newReviewImage = await ReviewImage.create({
     reviewId,
     url,
   });
 
-  console.log(newReviewImage);
   result.id = newReviewImage.id;
   result.url = newReviewImage.url;
-console.log(result)
-  return res.status(200).json();
+
+  return res.status(200).json(result);
 });
 
 module.exports = router;
