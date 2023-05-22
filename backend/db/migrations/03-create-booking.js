58825eb8e7162fb73b89110b30a74c28f82d030c
ALDuBose <alr.dubose@gmail.com>
2023-05-22 11:17:10 -0500

    updates to 49

--- backend/db/migrations/03-create-booking.js
+++ backend/db/migrations/03-create-booking.js
@@ -27,17 +27,9 @@ module.exports = {
         },
         startDate: {
           type: Sequelize.DATE,
-          // get: function() { // or use get(){ }
-          //   return this.getDataValue('startDate')
-          //     .toLocaleString('en-GB', { timeZone: 'UTC' });
-          // }
         },
         endDate: {
           type: Sequelize.DATE,
-          // get: function() { // or use get(){ }
-          //   return this.getDataValue('endDate')
-          //     .toLocaleString('en-GB', { timeZone: 'UTC' });
-          // }
         },
         createdAt: {
           allowNull: false,
