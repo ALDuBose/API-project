d30ca6a6a6025c6f32259e350fda86b84b90a742
ALDuBose <alr.dubose@gmail.com>
2023-05-22 14:04:59 -0500

    live testing of 58 complete

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,14 +1,51 @@
 const express = require("express");
 
 const { requireAuth } = require("../../utils/auth");
-const { Booking } = require("../../db/models");
+const {
+  Booking,
+  Review,
+  ReviewImage,
+  Spot,
+  SpotImage,
+  User,
+  sequelize,
+} = require("../../db/models");
 
-const { check } = require("express-validator");
-const { handleValidationErrors } = require("../../utils/validation");
+const {
+  handleValidationErrors,
+  requireAuth,
+} = require("../../utils/validation");
 
 const router = express.Router();
 
+router.get("/current", requireAuth, async (req, res, next) => {
+  const { user } = req;
+  let resultBooking = [];
 
+  const bookingData = await Booking.findAll({
+    where: { userId: user.id },
+  });
 
+  const spotData = await Spot.findAll({
+    where: { ownerId: user.id },
+    include: { model: SpotImage },
+  });
+
+  if (!bookingData)
+    return res.status(404).json({
+      message: "Booking couldn't be found",
+    });
+  for (let key in bookingData) {
+    currObj = {};
+    let currBooking = bookingData[key].toJSON();
+    let currSpot = spotData[key].toJSON();
+    currSpot.previewImage = currSpot.SpotImages;
+    delete currSpot.SpotImages;
+    currObj["Bookings"] = currBooking;
+    currObj["Bookings"].Spot = currSpot;
+    resultBooking.push(currObj);
+  }
+  return res.status(200).json(resultBooking);
+});
 
 module.exports = router;
